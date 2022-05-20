f3aad39596c3e5cc56f7475ca1582d283b368d3d
Hikmat Jafarli <jafarlihi@gmail.com>
2022-05-20 18:11:21 +0400

    Implement ISRs

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,9 +2,12 @@
 
 #include <kernel/tty.h>
 #include <arch/i386/gdt.h>
+#include <arch/i386/idt.h>
 
 void kernel_main(void) {
     terminal_initialize();
     init_gdt();
-    printf("GDT initialized");
+    printf("GDT initialized\n");
+    init_idt();
+    printf("IDT initialized\n");
 }
