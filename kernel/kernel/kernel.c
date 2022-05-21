0c2dc467290693b8cacb5a99a8372bacbc8d5023
Hikmat Jafarli <jafarlihi@gmail.com>
2022-05-21 14:05:05 +0400

    Implement basic frame allocator

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include <arch/i386/multiboot.h>
 #include <arch/i386/gdt.h>
 #include <arch/i386/idt.h>
 #include <arch/i386/irq.h>
 #include <arch/i386/timer.h>
 #include <arch/i386/keyboard.h>
+#include <arch/i386/mm.h>
 
-void kernel_main(void) {
+void kernel_main(uint32_t mboot_magic, void *mboot_header) {
     terminal_initialize();
+
+    if (mboot_magic != MULTIBOOT_BOOTLOADER_MAGIC) {
+        printf("Error: We weren't booted by a compliant bootloader!\n");
+        return;
+    }
+
+    multiboot_info_t *mboot_hdr = (multiboot_info_t *)mboot_header;
+
+    if ((mboot_hdr->flags & (1<<6)) == 0) {
+        printf("Error: No Multiboot memory map was provided!\n");
+        return;
+    }
+
     init_gdt();
     printf("GDT initialized\n");
     init_idt();
@@ -26,6 +41,11 @@ void kernel_main(void) {
     printf("Waiting 100 ticks\n");
     timer_wait(100);
     printf("Finished waiting\n");
+
+    init_mm(mboot_hdr, (uint32_t)mboot_hdr, (uint32_t)(mboot_hdr + sizeof(multiboot_info_t)));
+    uint32_t new_frame = allocate_frame();
+    uint32_t new_frame_addr = mmap_read(new_frame, MMAP_GET_ADDR);
+    printf("Frame allocated at: 0x%x\n", new_frame_addr);
 
     while (1) {}
 }
